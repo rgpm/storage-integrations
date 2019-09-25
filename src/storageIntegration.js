421ebd9fe8d2df766277d18e0bf6914eae15684f
Owen Parkins <oparkins@gmail.com>
2019-09-25 12:38:11 -0600

    Created the main interface and tests. Closed #1

---
+++ src/storageIntegration.js
@@ -0,0 +1,36 @@
+"use strict"
+
+const NotImplementedError = require("./notImplementedError");
+
+class StorageIntegration {
+    authorize() {
+        throw new NotImplementedError("StorageIntegration: authorize: not implemented");
+    }
+
+    unauthorize() {
+        throw new NotImplementedError("StorageIntegration: unauthorize: not implemented");
+    }
+
+    createFile() {
+        throw new NotImplementedError("StorageIntegration: createFile: not implemented");
+    }
+
+    readFile() {
+        throw new NotImplementedError("StorageIntegration: readFile: not implemented");
+    }   
+
+    updateFile() {
+        throw new NotImplementedError("StorageIntegration: updateFile: not implemented");
+    }
+
+    deleteFile() {
+        throw new NotImplementedError("StorageIntegration: deleteFile: not implemented");
+    }
+
+    check() {
+        throw new NotImplementedError("StorageIntegration: check: not implemented");
+    }
+}
+
+module.exports = StorageIntegration;
+
--- test/node/storageIntegration.test.js
+++ test/node/storageIntegration.test.js
@@ -1,13 +1,13 @@
 describe("StorageIntegration", () => {
     it("should be a class", () => {
-        const StorageIntegration = require("../src/storageIntegration");
+        const StorageIntegration = require("../../src/storageIntegration");
         expect(typeof StorageIntegration).toBe("function");
         // Classes are not callable; functions are
         expect(StorageIntegration).toThrow(TypeError);
     });
 
     it("should produce an object with new", () => {
-        const StorageIntegration = require("../src/storageIntegration");
+        const StorageIntegration = require("../../src/storageIntegration");
         expect(new StorageIntegration()).toBeInstanceOf(Object);
     })
 });
@@ -15,8 +15,9 @@ describe("StorageIntegration", () => {
 
 describe("StorageIntegration methods", () => {
     let StorageIntegration = undefined;
+    const NotImplementError = require("../../src/notImplementedError")
     beforeEach(() => {
-        StorageIntegrationLib = require("../src/storageIntegration");
+        StorageIntegrationLib = require("../../src/storageIntegration");
         StorageIntegration = new StorageIntegrationLib();
     });
 
@@ -24,35 +25,69 @@ describe("StorageIntegration methods", () => {
         it("should exist", () => {
             expect(StorageIntegration.authorize).toBeDefined();
         });
+
+        it("should throw an error", () =>{
+            expect(StorageIntegration.authorize).toThrow(NotImplementError);
+        })
     });
 
     describe("unauthorize method", () => {
         it("should exist", () => {
             expect(StorageIntegration.unauthorize).toBeDefined();
         });
+
+        it("should throw an error", () =>{
+            expect(StorageIntegration.unauthorize).toThrow(NotImplementError);
+        })
     });
 
     describe("createFile method", () => {
         it("should exist", () => {
             expect(StorageIntegration.createFile).toBeDefined();
         });
+
+        it("should throw an error", () =>{
+            expect(StorageIntegration.createFile).toThrow(NotImplementError);
+        })
     });
 
     describe("readFile method", () => {
         it("should exist", () => {
             expect(StorageIntegration.readFile).toBeDefined();
         });
+
+        it("should throw an error", () =>{
+            expect(StorageIntegration.readFile).toThrow(NotImplementError);
+        })
     });
 
     describe("updateFile method", () => {
         it("should exist", () => {
             expect(StorageIntegration.updateFile).toBeDefined();
         });
+
+        it("should throw an error", () =>{
+            expect(StorageIntegration.updateFile).toThrow(NotImplementError);
+        })
     });
 
     describe("deleteFile method", () => {
         it("should exist", () => {
             expect(StorageIntegration.deleteFile).toBeDefined();
         });
+
+        it("should throw an error", () =>{
+            expect(StorageIntegration.deleteFile).toThrow(NotImplementError);
+        })
     });
+
+    describe("check method", () => {
+        it("should exist", () => {
+            expect(StorageIntegration.check).toBeDefined();
+        });
+
+        it("should throw an error", () =>{
+            expect(StorageIntegration.check).toThrow(NotImplementError);
+        })
+    })
 });
